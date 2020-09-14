bf1519cdd6d13f6a7896b41ec77ca48e065349f2
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-09-14 15:33:38 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './config/database';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('server ativo na porta:3333');
+  console.log('👻 server started on port 3333!');
 });
