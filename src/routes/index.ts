2ddae304d301019adb51497eb2e3d6feab7f507e
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-09-18 20:28:07 -0300

    change routing structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from './users.routes';
 import serviceRouter from './services.routes';
 import providerRouter from './provider.routes';
+import schedulingRouter from './scheduling.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/services', serviceRouter);
 routes.use('/provider', providerRouter);
+routes.use('/scheduling', schedulingRouter);
 
 export default routes;
--- src/routes/scheduling.routes.ts
+++ src/routes/scheduling.routes.ts
@@ -1,1 +1,13 @@
 import { Router } from 'express';
+
+import SchedulingController from '../controllers/SchedulingController/SchedulingController';
+
+const schedulingRouter = Router();
+
+schedulingRouter.post('/', SchedulingController.store);
+schedulingRouter.get('/', SchedulingController.index);
+schedulingRouter.delete('/:id', SchedulingController.delete);
+schedulingRouter.put('/:id', SchedulingController.update);
+schedulingRouter.get('/:id', SchedulingController.show);
+
+export default schedulingRouter;
