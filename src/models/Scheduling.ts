5ea6241e130d98790afdc16609aac0eb4f772de1
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-09-18 20:58:42 -0300

    id_service in model

--- src/models/Scheduling.ts
+++ src/models/Scheduling.ts
@@ -6,6 +6,7 @@ const schedulingSchema = new Schema({
   id: { type: String, required: true },
   id_user: { type: String, required: true },
   id_provider: { type: String, required: true },
+  id_service: { type: String, required: true },
   scheduled_date: { type: String, required: true },
   appointment: { type: String, required: true },
   description: { type: String, required: true },
