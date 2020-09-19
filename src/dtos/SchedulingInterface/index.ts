bf6e7fd7c85e65234f157e2bcc0ffd6c5059d1b7
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-09-18 21:05:01 -0300

    id -> name

--- src/dtos/SchedulingInterface/index.ts
+++ src/dtos/SchedulingInterface/index.ts
@@ -2,11 +2,11 @@ import { Document } from 'mongoose';
 
 export interface IScheduling extends Document {
   id: string;
-  id_user: string;
-  id_provider: string;
-  id_service: string;
+  name_user: string;
+  name_provider: string;
+  name_service: string;
   scheduled_date: string;
   appointment: string;
-  description: string;
+  obsertation: string;
   canceled: boolean;
 }
--- src/models/Scheduling.ts
+++ src/models/Scheduling.ts
@@ -4,12 +4,12 @@ import { IScheduling } from '../dtos';
 
 const schedulingSchema = new Schema({
   id: { type: String, required: true },
-  id_user: { type: String, required: true },
-  id_provider: { type: String, required: true },
-  id_service: { type: String, required: true },
+  name_user: { type: String, required: true },
+  name_provider: { type: String, required: true },
+  name_service: { type: String, required: true },
   scheduled_date: { type: String, required: true },
   appointment: { type: String, required: true },
-  description: { type: String, required: true },
+  obsertation: { type: String, required: true },
   canceled: { type: Boolean, required: false },
 });
 
